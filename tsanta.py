ca6ed32ced75113ab0e340c691016b26917e1126
tsanta-wq <millihenri1@gmail.com>
2026-06-04 21:48:26 +0300

    Change Facebook pseudo from Tsanta.Niaina to Sv.Cv

--- tsanta.py
+++ tsanta.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template_string
 app = Flask(__name__)
 
 # CONFIGURATION DE TON COMPTE FACEBOOK
-FACEBOOK_PSEUDO = "Tsanta.Niaina"
+FACEBOOK_PSEUDO = "Sv.Cv"
 
 HTML_TEMPLATE = """
 <!DOCTYPE html>
